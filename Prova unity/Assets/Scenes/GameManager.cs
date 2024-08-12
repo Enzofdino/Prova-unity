52a4fbae7dae13968454996449f349bac05d0008
Enzofdino <enzofhostin@gmail.com>
2024-08-12 10:41:50 -0300

    variaveis

--- Prova unity/Assets/Scenes/GameManager.cs
+++ Prova unity/Assets/Scenes/GameManager.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    static public GameManager instance;
+    int cordavez, acertos, erros;
+    [SerializeField] int[] segencia;
+    [SerializeField] string[] nomes;
+    #region
+    private void Awake()
     {
-        
+        instance = this;
     }
+    #endregion
 }
