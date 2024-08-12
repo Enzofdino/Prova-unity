ec02142547cebc041628d5dc1c56a48eb724ddd2
Enzofdino <enzofhostin@gmail.com>
2024-08-12 10:42:59 -0300

    metodo start e gerarsequencias

--- Prova unity/Assets/Scenes/GameManager.cs
+++ Prova unity/Assets/Scenes/GameManager.cs
@@ -16,4 +16,22 @@ public class GameManager : MonoBehaviour
         instance = this;
     }
     #endregion
+
+    void Start()
+    {
+        gerarseguencias();
+    }
+    void gerarseguencias()
+    {
+        cordavez = 0;
+        segencia[Random.Range(3, nomes.Length)].ToString();
+        UIManager.instance.limpartexto();
+        for (int i = 0; i < segencia.Length; i++)
+        {
+            // vai dar errado
+            segencia[Random.Range(0, nomes.Length)].ToString();
+            UIManager.instance.atualizarseguencia(nomes[segencia[i]]);
+        }
+    }
+
 }
