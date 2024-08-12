6689aad432cd735bb6a8e52bd23ad3cf09c45003
Enzofdino <enzofhostin@gmail.com>
2024-08-12 10:48:59 -0300

    variaveis

--- Prova unity/Assets/Scenes/UIManager.cs
+++ Prova unity/Assets/Scenes/UIManager.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
+    //sington 
+    #region 
+    static public UIManager instance;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        instance = this;
     }
+    #endregion
+    [SerializeField] Button[] butons;
+    [SerializeField] TextMeshProUGUI segenciatexto, acertotexto, erroutexto;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
