84060c7a67cd7c08083df7cd3a6d767556fe055d
Enzofdino <enzofhostin@gmail.com>
2024-08-12 10:55:17 -0300

    na branch main o GameManager

--- Prova unity/Assets/Scripts/GameManager.cs
+++ Prova unity/Assets/Scripts/GameManager.cs
@@ -5,14 +5,56 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    static public GameManager instance;
+    int cordavez, acertos, erros;
+    [SerializeField] int[] segencia;
+    [SerializeField] string[] nomes;
+    #region
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
     void Start()
     {
-        
+        gerarseguencias();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    void gerarseguencias()
+    {
+        cordavez = 0;
+        segencia[Random.Range(3, nomes.Length)].ToString();
+        UIManager.instance.limpartexto();
+        for (int i = 0; i < segencia.Length; i++)
+        {
+            // vai dar errado
+            segencia[Random.Range(0, nomes.Length)].ToString();
+            UIManager.instance.atualizarseguencia(nomes[segencia[i]]);
+        }
+    }
+    public void checarcor(int corindex)
+    {
+        if (cordavez == corindex)
+        {
+            cordavez++;
+        }
+        if (cordavez == segencia.Length)
+        {
+            acertos++;
+            UIManager.instance.atualizaracertos(acertos);
+            gerarseguencias();
+        }
+        if (cordavez != segencia.Length)
+        {
+            erros++;
+            UIManager.instance.atualizarerros(erros);
+            gerarseguencias();
+        }
+    }
+
 }
